7231b5251033bf95ed4edceeefb9bc7936c98757
Chris Harvey <chharvey@icloud.com>
2018-03-09 22:29:29 +0000

    signup form

--- app/controllers/users.controller.rb
+++ app/controllers/users.controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
   use Rack::Flash
+
+  get '/signup' do
+    if logged_in?
+      redirect '/home'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      user = User.create(params)
+      user.save
+      session[:user_id] = @user.id
+      redirect '/home'
+    end
+  end
+
+
+
+
+
 end
