d266d95fabc81756d7193bb9e4fee9dd3a41a4ac
Chris Harvey <chharvey@icloud.com>
2018-03-11 04:10:03 +0000

    lists controller basics

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,43 @@
 class ListsController < ApplicationController
   use Rack::Flash
+
+
+  get '/lists' do
+
+  end
+
+
+  post '/lists' do
+
+  end
+
+
+  get '/lists/new' do
+
+  end
+
+
+  get '/lists/:id' do
+
+  end
+
+
+  get '/lists/:id/edit' do
+
+  end
+
+
+  patch '/lists/:id' do
+
+  end
+
+
+  get '/lists/:id/delete' do
+
+  end
+
+
+  delete '/lists/:id/delete' do
+
+  end
 end
